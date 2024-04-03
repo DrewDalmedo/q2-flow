c5965199940152dac85816b69ff4bf33014c8963
Drew Dalmedo <andrewdalmedo@gmail.com>
2024-04-03 16:19:08 -0400

    Check if bolt has been successfully created

--- src/game/g_weapon.c
+++ src/game/g_weapon.c
@@ -459,6 +459,8 @@ fire_blaster(edict_t *self, vec3_t start, vec3_t dir, int damage,
 	VectorNormalize(dir);
 
 	bolt = G_Spawn();
+  if (!bolt) return;
+
 	bolt->svflags = SVF_DEADMONSTER;
 
 	/* yes, I know it looks weird that projectiles are deadmonsters
